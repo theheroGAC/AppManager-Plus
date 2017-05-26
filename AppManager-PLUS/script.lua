b57e3a7756efdec5f5d6a875e7a5d64496554056
ONELua Team <oneluaproject@gmail.com>
2017-05-26 10:33:24 -0500

    v2.20

--- AppManager-PLUS/script.lua
+++ AppManager-PLUS/script.lua
@@ -8,21 +8,18 @@
 	Designed By Gdljjrod & DevDavisNunez.
 	Collaborators: BaltaR4 & Wzjk.
 ]]
-
 color.loadpalette()
 
 files.mkdir("ux0:data/AppManPlus/themes/")								-- Create a folder work
 
 -- Global constants
 APP_VERSION_MAJOR = 0x02 -- major.minor
-APP_VERSION_MINOR = 0x10
+APP_VERSION_MINOR = 0x20
 	
 APP_VERSION = ((APP_VERSION_MAJOR << 0x18) | (APP_VERSION_MINOR << 0x10)) -- Union Binary
 
 -- == Updater ==
 game.delete("AMUPDATER") -- Exists delete update app
-
-files.mkdir("ux0:/data/1luapkg")
 
 function onAppInstallCB(step, size_argv, written, file, totalsize, totalwritten)
 	return 10 -- Ok code
@@ -35,7 +32,7 @@ function onNetGetFileCB(size,written,speed)
 	draw.fillrect(0,520,((written*960)/size),24,color.new(0,255,0))
 	screen.flip()
 	buttons.read()
-	if buttons.circle then	return 0 end --Cancel or Abort
+	if buttons.circle then return 0 end --Cancel or Abort
 	return 1;
 end
 
@@ -45,6 +42,7 @@ if version then
 	local major = (version >> 0x18) & 0xFF;
 	local minor = (version >> 0x10) & 0xFF;
 	if version > APP_VERSION then
+		files.mkdir("ux0:/data/1luapkg")
 		local res = os.message("App Manager Plus "..string.format("%X.%02X",major, minor).." is now available.\n".."Do you want to update the application?", 1)
 		if res == 1 then -- Response Ok.
 			local url = string.format("https://github.com/ONElua/AppManager-PLUS/releases/download/%s/AppManagerPlus.vpk", string.format("%X.%02X",major, minor))
@@ -52,18 +50,24 @@ if version then
 			--os.message(url:sub(1,#url/2).."\n"..url:sub(#url/2))
 			onAppInstall = onAppInstallCB
 			onNetGetFile = onNetGetFileCB
-			local res = http.getfile(url, path)
+			buttons.homepopup(0)
+				local res = http.getfile(url, path)
+			buttons.homepopup(1)
 			if res then -- Success!
 				files.copy("eboot.bin","ux0:/data/1luapkg")
 				files.copy("updater/script.lua","ux0:/data/1luapkg/")
 				files.copy("updater/param.sfo","ux0:/data/1luapkg/sce_sys/")
-				game.installdir("ux0:/data/1luapkg")
+				buttons.homepopup(0)
+					game.installdir("ux0:/data/1luapkg")
+				buttons.homepopup(1)
 				files.delete("ux0:/data/1luapkg")
 				game.launch("AMUPDATER") -- Goto installer extern!
 			end
 		end
 	end
 end
+
+buttons.homepopup(1)
 
 dofile("system/utils.lua") 												-- Extra funtions
 
