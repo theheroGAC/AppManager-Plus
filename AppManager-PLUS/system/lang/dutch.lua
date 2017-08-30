6a725a972fcdd0c01563a275ff44c2463e708897
ONELua Team <oneluaproject@gmail.com>
2017-08-30 09:29:42 -0500

    Update v3.01

---
+++ AppManager-PLUS/system/lang/dutch.lua
@@ -0,0 +1,169 @@
+--Texts
+strings = {
+
+-------------	Explorer	---------------------------------------------------------------------------------
+back =			"Vorige " ",
+copyfile =		"Kopiëren: ",
+delfile =		"Verwijderen: ",
+dir =			"<DIR>",
+creatfolder =	"Map aanmaken",
+newfolder =		"Nieuwe map",
+pass =			"Gebruik Pass ?",
+ospass =		"PASS",
+
+--SubMenu
+copy =			"Kopieer",
+delete =		"Verwijderen",
+makedir =		"MakeDir",
+rename =		"Hernoemen",
+extract =		"Uitpakken",
+cancel =		"Annuleren",
+insvpkfromdir = "Installeer Game",
+instheme =		"Installeer CTheme",
+extractto =		"Uitpakken naar",
+paste =			"Plakken",
+move =			"Verplaatsen",
+size =			"Grootte",
+scanvpks =		"Content Scanner (VPK/ISO/CSO)",
+export =		"Export",
+themes =		"Themes AppManager+",
+pluginsman =	"Plugins Man",
+cthemesman =	"LiveArea CustomThemes",
+ftp =			"FTP",
+restarthb =		"Herstart AppMan",
+reset =			"Power Reset",
+off =			"Uitschakelen",
+
+items =			"Sel Items: ",
+
+------------	Ftp 		---------------------------------------------------------------------------------
+textftp =		"Gebruik dit FTP adres op jouw toestel of PC",
+closeftp =		"Klik op START om de verbinding te sluiten",
+loseftp =		"FTP Verbinding afgesloten",
+
+------------	USB 		---------------------------------------------------------------------------------
+usb =			"USB",
+connectusb =	"Verbind een USB kabel",
+usbconnection = "USB Verbinding geactiveerd",
+cancelusb =		"Uitgeschakeld",
+
+imgerror =		"Error Image not supported",
+
+-- ## Music Player ##
+playing =		"Playing",
+paused =		"Gepauzeerd",
+time =			"Tijd: ",
+tags =			"xx:xx:xx",
+sounderror =	"Probleem met geluid",
+
+--AppManager
+apptittle =		"Game Manager (Apps - Bubbles)",
+switch =		"Really wish to switch ",
+nomemory =		"Niet genoeg geheugen...",
+appremove =		"Ik wil dit echt verwijderen ",
+delclon	=		"Verwijder deze CLON: ",
+manual =		"Wil je de handleiding verwijderen?",
+notmanual =		"Handleiding is verwijderd",
+notfindmanual =	"Handleiding bestaat niet",
+removemanual =	"Verwijder handleiding",
+pressremove =	"Verwijderen",
+switchapp =		"Switch Apps",
+empty =			"Leeg/Error",
+toexplorer =	"Explorer",
+partitions =	"Partitities",
+
+--SearchingVpks
+vpktittle =		"Vpks zoeken",
+wait =			"Even geduld",
+nofinds =		"Geen bestanden gevonden",
+reloadfull =	"Volledige Scan van ux0/ur0/uma0",
+moveiso2ux0 =	"Verplaatst Game naar ux0:pspemu/ISO",
+moveiso2ur0 =	"Verplaatst Game naar ur0:pspemu/ISO",
+moveiso2uma0 =	"MVerplaatst Game naar uma0:pspemu/ISO",
+
+-------------		Callbacks		-------------------------------------------------------------------------
+unsafe =		"Onveilig: ",
+file =			"Bestand ",
+percent =		"Percent: ",
+percent_total = "Percent Total: ",
+total_size =	"Volledige grootte: ",
+total_written = "Totaal geschreven: ",
+speed =			"Snelheid: ",
+remaining =		"Resterend: ",
+
+--onAppInstall
+searchunsafe =	"Zoek in VPK naar onveilige of gevaarlijke bestanden.",
+unsafevpk =		"Uitgebreide permissies en persoonlijke info vereist!\n\nWees voorzichtig met onbekende pakketen \n\nWil je dit op eigen risico installeren?",
+dangerousvpk =	"Waarschuwing!!! Groot risico op een brick van je toestel als je deze VPK installeert! \n\nGa voorzichtig om met niet geverifieerde packages! \n\nWil je dit op eigen risico installeren?",
+vpkunpack =		"VPK uitpakken...",
+install =		"Installeren...",
+
+--onExtractFiles
+extraction =	"Uitpakken...",
+
+--onScanningFiles
+scanning =		"Scanning...",
+size_unpk =		"Uitgepakte grootte: ",
+-------------------------------------------------------------------------------------------------------------
+
+--Launch PBP or VPK
+launchpbp =		"Start game/app ",
+confirm =		"Accepteren",
+warningvpk =	"\nWAARSCHUWING TOTAL ACCESS\nUNSAFE VPK",
+warningvpk2 =	"WARNING TOTAL ACCESS",
+safevpk =		"Veilige VPK",
+total_sizevpk = "Totale grootte VPK: ",
+installvpk =	"INSTALLEER VPK",
+installgame =	"INSTALLEER APP",
+alertunsafe =	"ONVEILIGE VPK!!!!",
+alertdang =		"GEVAARLIJKE VPK!!!!",
+installvpkdir =	"INSTALLEER VPK UIT MAP",
+launchbin =		"Start app ",
+
+------------	Switch Apps/vpks	-------------------------------------------------------------------------
+required =		"\nVereist: ",
+searchapp =		"\nWil je de app verplaatsen naar ur0 ?",
+vpktour0 =		"Wil je deze vpk verplaatsen naar ur0: so you can install it ?",
+vpktoux0 =		"Wil je deze vpk verplaatsen naar to ux0: ?",
+stillnomemory =	"Niet genoeg vrije ruimte wanneer u verplaatst",
+vpkinuro =		"The vpk bestaat al op de locatie ur0: \nProbeer meer vrije ruimte te maken...",
+apptour0 =		"Verplaats naar ur0:",
+ur0toapp =		"Verplaats app to ux0: ?",
+tittle =		"Titel: ",
+errorapp =		"Fout...app kan niet worden verplaatst...",
+errorinstall =	"Er is iets foutgelopen tijdens de installatie",
+
+------------	ThemesAppmanager	-------------------------------------------------------------------------
+themesappman =	"Themas voor AppManager PLUS",
+themesdone =	"Installatie voltooid!",
+reload =		"Herlaad het originele Thema",
+settinsthemes =	"Ga naar Thema instellingen ?",
+
+------------	PluginsManager		-------------------------------------------------------------------------
+plugsmanager =	"Plugins Manager",
+reloadconfig =	"TaiHen Config",
+configtxt =		"Herlaad taiHEN ConfigTxt",
+disablesuprx =	"Disable plugin ?",
+
+------------	ThemesLivearea		-------------------------------------------------------------------------
+customthemes =	"CustomThemes LiveArea",
+notthemes =		"Leeg/Fout in theme list!",
+deltheme =		"Verwijder CustomTheme from app.db",
+delfilestheme =	"Verwijder bestanden uit CustomTheme ?",
+
+------------	Advanced	---------------------------------------------------------------------------------
+advanced =		"Geadvanceerd",
+refreshdb =		"Update app.db",
+rebuilddb =		"Rebuild app.db",
+restartupdb =	"Jouw PSVita zal herstarten...\nand de data base wordt geupdate",
+restartredb =	"Jouw PSVita zal herstarten...\nand de data base wordt gereconstrueerd",
+restart =		"Jouw PSVita zal herstarten...",
+shutdown =		"Jouw PSVita zal afsluiten...",
+
+result =		"\nResult: ",
+count =			"Count: ",
+sizeis =		"Grootte is: ",
+opensettings =	"Open Mp3 Player/Gallerij/Video ?",
+filesfolders =	"Bestanden/Mappen",
+
+}
